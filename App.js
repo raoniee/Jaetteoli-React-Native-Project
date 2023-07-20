5fa5e21975b2d10297cf2cff8774124a51970dae
한지은 <hjn510450@gmail.com>
2023-07-21 00:14:41 +0900

    기본 설정

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Nav from './components/common/Nav';
+import Home from './components/home/Home';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home></Home>
+      <Nav></Nav>
       <StatusBar style="auto" />
     </View>
   );
