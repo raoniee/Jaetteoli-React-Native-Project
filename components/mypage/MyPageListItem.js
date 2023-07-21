8d958d110eaeaadcc99d94fa41fb9cfa14b35b51
한지은 <hjn510450@gmail.com>
2023-07-21 20:44:10 +0900

    마이떨이 UI 제작

---
+++ components/mypage/MyPageListItem.js
@@ -0,0 +1,30 @@
+import { StyleSheet, View, Text } from 'react-native';
+import Color from '../../assets/colors/Color';
+import ArrowRight from '../../assets/images/ArrowRight';
+
+const MyPageListItem = (props) => {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.text}>{props.name}</Text>
+            <ArrowRight stroke={Color.lightGray}></ArrowRight>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '93%',
+        height: 35,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 400,
+        lineHeight: 35,
+        marginLeft: 10,
+    },
+})
+
+export default MyPageListItem;
