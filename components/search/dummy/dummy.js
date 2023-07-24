b931d05b3f85e4334718ca308415e6fbb977285e
Anwooseong <aws5624@naver.com>
2023-07-24 13:07:42 +0900

    검색하기 Dummy

---
+++ components/search/dummy/dummy.js
@@ -0,0 +1,71 @@
+export const recentSearchData = [
+    {
+      key: "1",
+      name: "bbq",
+    },
+    {
+      key: "2",
+      name: "요달",
+    },
+    {
+      key: "3",
+      name: "찜닭",
+    },
+    {
+      key: "4",
+      name: "떡볶이",
+    },
+    {
+      key: "5",
+      name: "피자스쿨",
+    },
+    {
+      key: "6",
+      name: "맘스터치",
+    },
+  ];
+
+
+  export const popularSearchData = [
+    {
+      key: "1",
+      name: "타코야끼",
+    },
+    {
+      key: "2",
+      name: "육회",
+    },
+    {
+      key: "3",
+      name: "던킨도너츠",
+    },
+    {
+      key: "4",
+      name: "닭발",
+    },
+    {
+      key: "5",
+      name: "만두",
+    },
+    {
+      key: "6",
+      name: "물회",
+    },
+    {
+      key: "7",
+      name: "닭꼬치",
+    },
+    {
+      key: "8",
+      name: "샐러드",
+    },
+    {
+      key: "9",
+      name: "샌드위치",
+    },
+    {
+      key: "10",
+      name: "치킨",
+    },
+  ];
+  
