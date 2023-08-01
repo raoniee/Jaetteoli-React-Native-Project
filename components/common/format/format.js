df52e589494d471949a15d331485e0276270e2a7
Anwooseong <aws5624@naver.com>
2023-08-01 10:23:46 +0900

    format.js 파일 이동

--- components/common/format/format.js
+++
@@ -1,8 +0,0 @@
-export function formatPriceWithCurrency(price) {
-    if(price === undefined){
-      return ''
-    }
-    const formattedPrice = price.toLocaleString();
-  
-    return formattedPrice + '원';
-  }
