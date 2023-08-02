ef76d3d934f1976b31f4f105a8ab14a92ef2c1b1
한지은 <hjn510450@gmail.com>
2023-08-02 15:05:20 +0900

    리뷰 쓰기 페이지 사진 업로드 기능 추가(미디어 라이브, 카메라 권한 팝업 기능 추가)

--- pages/review/WriteReview.js
+++ pages/review/WriteReview.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ActionSheetIOS, SafeAreaView } from 'react-native';
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Color from '../../assets/colors/Color';
@@ -6,6 +6,9 @@ import Camera from '../../assets/images/Camera';
 import Button from '../../components/common/Button';
 import StarStroke from '../../assets/images/StarStroke';
 import Header from '../../components/common/Header';
+import * as ImagePicker from 'expo-image-picker';
+import Close from '../../assets/images/Close';
+import Modal from 'react-native-modal';
 
 const WriteReview = () => {
 
@@ -27,10 +30,125 @@ const WriteReview = () => {
 
     const [starRating, setStarRating] = useState(0);
 
+    // 미디어 라이브러리(사진 보관함) 권한 상태, 권한 요청 함수
+    const [mediaLibrary, requestMediaLibraryPermission] = ImagePicker.useMediaLibraryPermissions();
+    // 카메라 권한 상태, 권한 요청 함수
+    const [camera, requestCameraPermission] = ImagePicker.useCameraPermissions();
+
+    // 미디어 라이브러리(사진 보관함) 열기
+    const openMediaLibrary = async () => {
+
+        // 미디어 라이브러리(사진 보관함) 권한이 정해지지 않은 경우 권한 요청 함수 실행
+        if (mediaLibrary.status === 'undetermined') {
+            const permission = await requestMediaLibraryPermission();
+            if (!permission.granted) {
+                setModalText({
+                    title: `사진 보관함 접근 권한 허용을 안 하시면\n사진 보관함 내 사진에 접근이 불가능합니다.`,
+                    contents: `사진 보관함 접근 허용을 원하시면 휴대폰 설정에서\n재떨이의 사진 보관함 접근 권한을 허용해 주세요.`
+                })
+                setModalVisible(true);
+                return;
+            }
+        }
+
+        // 미디어 라이브러리(사진 보관함) 권한이 없을 경우 리턴
+        if (mediaLibrary.status === 'denied') {
+            setModalText({
+                title: `사진 보관함 접근 권한 허용을 안 하시면\n사진 보관함 내 사진에 접근이 불가능합니다.`,
+                contents: `사진 보관함 접근 허용을 원하시면 휴대폰 설정에서\n재떨이의 사진 보관함 접근 권한을 허용해 주세요.`
+            })
+            setModalVisible(true);
+            return;
+        }
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setUserProfile(result.assets[0].uri)
+        }
+    };
+
+    // 카메라로 사진 찍기
+    const takePhoto = async () => {
+        // 권한 확인 코드: 권한 없으면 물어보고, 승인하지 않으면 함수 종료
+        if (camera.status === 'undetermined') {
+            const permission = await requestCameraPermission();
+            if (!permission.granted) {
+                setModalText({
+                    title: `카메라 접근 권한 허용을 안 하시면 사진 촬영이 불가능합니다.`,
+                    contents: `사진 촬영을 원하시면 휴대폰 설정에서\n재떨이의 카메라 접근 권한을 허용해 주세요.`
+                })
+                setModalVisible(true);
+                return;
+            }
+        }
+
+        if (camera.status === 'denied') {
+            setModalText({
+                title: `카메라 접근 권한 허용을 안 하시면\n사진 촬영이 불가능합니다.`,
+                contents: `사진 촬영을 원하시면 휴대폰 설정에서\n재떨이의 카메라 접근 권한을 허용해 주세요.`
+            })
+            setModalVisible(true);
+            return;
+        }
+
+        let result = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setUserProfile(result.uri);
+        }
+    };
+
+    const showActionSheet = () => {
+        ActionSheetIOS.showActionSheetWithOptions(
+            {
+                options: ['사진 보관함', '사진 찍기', '파일 선택', '취소'],
+                cancelButtonIndex: 3,
+            },
+            (buttonIndex) => {
+                if (buttonIndex === 0) {
+                    openMediaLibrary();
+                } else if (buttonIndex === 1) {
+                    takePhoto();
+                } else if (buttonIndex === 2) {
+                    console.log('파일 선택 선택');
+                }
+            }
+        );
+    };
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalText, setModalText] = useState({
+        title: `사진 보관함 접근 권한 허용을 안 하시면\n사진 보관함 내 사진에 접근이 불가능합니다.`,
+        contents: `사진 보관함 접근 허용을 원하시면 휴대폰 설정에서\n재떨이의 사진 보관함 접근 권한을 허용해 주세요.`
+    });
+
+    const openAppSettings = () => {
+        if (Platform.OS === 'ios') {
+            Linking.openURL('app-settings:');
+        } else if (Platform.OS === 'android') {
+            Linking.openSettings();
+        }
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
+
+
     return (
         <SafeAreaView style={styles.container}>
             <Header left={2} right={0} backgroundColor={Color.white} title='리뷰쓰기' navigation={navigation} />
-            <View style={styles.container}> 
+            <View style={styles.container}>
                 <View style={styles.orderInfo}>
                     <Text style={styles.storeName}>{orderInfo.storeName}</Text>
                     <Text style={styles.orderMenu}>{orderInfo.orderMenu.map((orderMenu) => orderMenu.menuName).join(', ')}</Text>
@@ -77,13 +195,30 @@ const WriteReview = () => {
                     <TouchableOpacity onPress={() => { setStarRating(4) }}><StarStroke stroke={Color.black} fill={Color.yellow}></StarStroke></TouchableOpacity>
                     <TouchableOpacity onPress={() => { setStarRating(5) }}><StarStroke stroke={Color.black} fill={Color.yellow}></StarStroke></TouchableOpacity>
                 </View>}
-                <View style={styles.addPicture}>
+                <TouchableOpacity style={styles.addPicture} onPress={showActionSheet}>
                     <Camera fill={Color.purple}></Camera>
                     <Text style={styles.addPictureText}>사진 첨부하기</Text>
-                </View>
+                </TouchableOpacity>
                 <TextInput placeholder='음식에 대한 리뷰를 남겨주세요!' placeholderTextColor={Color.lightGray} multiline style={styles.review}></TextInput>
                 <Button title='완료' backgroundColor={Color.darkPurple} color={Color.white} margin='50 0 0 0' height={50}></Button>
             </View>
+            <Modal
+                isVisible={modalVisible}
+                onBackdropPress={closeModal}
+                style={{
+                    justifyContent: 'flex-end',
+                    margin: 0,
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <TouchableOpacity onPress={closeModal} style={styles.closeModal}>
+                        <Close></Close>
+                    </TouchableOpacity>
+                    <Text style={styles.modalTitle}>{modalText.title}</Text>
+                    <Text style={styles.modalContents}>{modalText.contents}</Text>
+                    <Button title='설정하러 가기' backgroundColor={Color.darkPurple} color={Color.white} height={62} onPress={openAppSettings} margin='0 0 50 0' />
+                </View>
+            </Modal>
         </SafeAreaView>
     );
 }
@@ -149,6 +284,40 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 30,
         textAlignVertical: 'top',
+    },
+    button: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: 580,
+        left: 20
+    },
+    modalContainer: {
+        backgroundColor: 'white',
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+    },
+    closeModal: {
+        width: '100%',
+        marginTop: 20,
+        marginBottom: 20,
+        alignItems: 'flex-end'
+    },
+    modalTitle: {
+        fontFamily: 'Pretendard-SemiBold',
+        fontSize: 18,
+        lineHeight: 25,
+    },
+    modalContents: {
+        marginTop: 20,
+        fontFamily: 'Pretendard-Medium',
+        fontSize: 15,
+        lineHeight: 20,
+        color: Color.darkGray,
+        marginBottom: 50,
     }
 
 })
