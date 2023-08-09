71bfc87de61ad77b90b2259a1c69a5ceb19419af
한지은 <hjn510450@gmail.com>
2023-08-09 18:03:19 +0900

    내가 쓴 리뷰 조회 api 연결

--- pages/mypage/ManageReview.js
+++ pages/mypage/ManageReview.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, FlatList, Image } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native'
 import React, { useState, useEffect } from 'react';
 import reveiwImg from '../../assets/images/reviewImg.png'
 import Color from '../../assets/colors/Color';
@@ -7,16 +7,20 @@ import Star from '../../assets/images/Star';
 import profile from '../../assets/images/profile.png';
 import Header from '../../components/common/Header';
 import { baseUrl, jwt } from "../../utils/baseUrl";
+import { useNavigation } from '@react-navigation/native';
 
 const ManageReview = () => {
 
+    // 사용자 리뷰 데이터
+    const [reviews, setReviews] = useState([]);
+    const [reviewCount, setReviewCount] = useState(0)
+
+    // 컴포넌트가 마운트 될 경우 내가 쓴 리뷰 조회 api 요청
     useEffect(() => {
-        getReviews() .then(reviewItems => {
-            setReviewCount(reviewItems.totalReviews);
-            setReviews(reviewItems.myReviews)
-        })
+        getReviews();
     }, []);
 
+    // 내가 쓴 리뷰 조회 api
     async function getReviews() {
 
         const requestOptions = {
@@ -30,21 +34,46 @@ const ManageReview = () => {
             const response = await fetch(`${baseUrl}/jat/app/reviews`, requestOptions);
             const data = await response.json();
 
-            if (!data.isSuccess) {
+            if (data.isSuccess) {
+                setReviewCount(data.result.totalReviews);
+                setReviews(data.result.myReviews);
+            } else {
                 console.log(data.message);
-                return;
             }
-            console.log(data.result)
-            return data.result;
         } catch (error) {
             console.log(error)
         }
     }
 
-    const [reviewCount, setReviewCount] = useState(0)
+    // 리뷰 삭제하기 api
+    async function deleteReview(reviewIdx) {
+        const requestBody = {
+            reviewIdx: reviewIdx,
+        };
 
-    // 사용자 리뷰 데이터
-    const [reviews, setReviews] = useState(null);
+        const requestOptions = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-ACCESS-TOKEN': jwt,
+            },
+            body: JSON.stringify(requestBody),
+        };
+        try {
+            const response = await fetch(`${baseUrl}/jat/app/reviews`, requestOptions);
+            const data = await response.json();
+
+            if (data.isSuccess) {
+                // 삭제 후 사용자 리뷰 데이터를 다시 불러옴
+                getReviews()
+            } else {
+                console.log(data.message);
+            }
+        } catch (error) {
+            console.log('서버가 아직 안켜져있습니다.')
+            console.log(error)
+        }
+    }
 
     const getDateDifference = (dateString) => {
         const currentDate = new Date();
@@ -67,66 +96,74 @@ const ManageReview = () => {
         }
     };
 
+    const navigation = useNavigation();
+
+    const moveToStoreDetailPage = (storeIdx) => {
+        navigation.navigate("StoreDetailPage", { storeIdx: storeIdx })
+    }
+
     const renderItem = ({ item }) => {
-        return(
-        <View style={styles.itemContainer}>
-            <View style={styles.reviewWrapper}>
-                <View style={styles.store}>
-                    <Text style={styles.storeText}>{item.storeName}</Text>
-                    <ArrowRight stroke={Color.black}></ArrowRight>
-                </View>
-                <View>
-                    <View style={styles.star}>
-                        <View style={{ width: 5 * 15, flexDirection: 'row', overflow: 'hidden', position: 'absolute' }}>
-                            <Star fill={Color.lightGray}></Star>
-                            <Star fill={Color.lightGray}></Star>
-                            <Star fill={Color.lightGray}></Star>
-                            <Star fill={Color.lightGray}></Star>
-                            <Star fill={Color.lightGray}></Star>
+        return (
+            <View style={styles.itemContainer}>
+                <View style={styles.reviewWrapper}>
+                    <TouchableOpacity onPress={() => moveToStoreDetailPage(item.storeIdx)}>
+                        <View style={styles.store}>
+                            <Text style={styles.storeText}>{item.storeName}</Text>
+                            <ArrowRight stroke={Color.black}></ArrowRight>
                         </View>
-                        <View style={{ width: item.star * 15, flexDirection: 'row', overflow: 'hidden' }}>
-                            <Star fill={Color.yellow}></Star>
-                            <Star fill={Color.yellow}></Star>
-                            <Star fill={Color.yellow}></Star>
-                            <Star fill={Color.yellow}></Star>
-                            <Star fill={Color.yellow}></Star>
+                    </TouchableOpacity>
+                    <View>
+                        <View style={styles.star}>
+                            <View style={{ width: 5 * 15, flexDirection: 'row', overflow: 'hidden', position: 'absolute' }}>
+                                <Star fill={Color.lightGray}></Star>
+                                <Star fill={Color.lightGray}></Star>
+                                <Star fill={Color.lightGray}></Star>
+                                <Star fill={Color.lightGray}></Star>
+                                <Star fill={Color.lightGray}></Star>
+                            </View>
+                            <View style={{ width: item.star * 15, flexDirection: 'row', overflow: 'hidden' }}>
+                                <Star fill={Color.yellow}></Star>
+                                <Star fill={Color.yellow}></Star>
+                                <Star fill={Color.yellow}></Star>
+                                <Star fill={Color.yellow}></Star>
+                                <Star fill={Color.yellow}></Star>
+                            </View>
+                            <Text style={styles.date}>{item.date}</Text>
                         </View>
-                        <Text style={styles.date}>{item.date}</Text>
                     </View>
+                    {item.reviewUrl && <Image source={{ uri: item.reviewUrl }} style={styles.reviewImg}></Image>}
+                    <Text style={styles.contents}>{item.contents}</Text>
                 </View>
-                {item.reviewUrl && <Image source={{ uri: item.reviewUrl}} style={styles.reviewImg}></Image>}
-                <Text style={styles.contents}>{item.contents}</Text>
-            </View>
-            <Text style={styles.delete}>삭제하기</Text>
-            <View style={styles.menuWrapper}>
-                {item.reviewMenus.map((menu, index) => (
-                    <View style={styles.menu} key={index}><Text style={styles.menuText}>{menu.menuName}</Text></View>
-                ))}
-            </View>
-            {item.sellerComment && <View style={styles.comment}>
-                <View style={styles.profile}>
-                    <Image source={profile} style={styles.profileImg}></Image>
-                    <Text style={styles.profileText}>사장님</Text>
+                <TouchableOpacity style={styles.delete}><Text style={styles.deleteText}>삭제하기</Text></TouchableOpacity>
+                <View style={styles.menuWrapper}>
+                    {item.reviewMenus.map((menu, index) => (
+                        <View style={styles.menu} key={index}><Text style={styles.menuText}>{menu}</Text></View>
+                    ))}
                 </View>
-                <Text style={styles.commentText}>{item.sellerComment}</Text>
+                {item.sellerComment && <View style={styles.comment}>
+                    <View style={styles.profile}>
+                        <Image source={profile} style={styles.profileImg}></Image>
+                        <Text style={styles.profileText}>사장님</Text>
+                    </View>
+                    <Text style={styles.commentText}>{item.sellerComment}</Text>
+                </View>
+                }
             </View>
-            }
-        </View>
         );
     }
 
     return (
         <SafeAreaView style={styles.container}>
             <Header title='리뷰관리' />
-            <FlatList 
-                data={reviews} 
-                renderItem={renderItem} 
+            <FlatList
+                data={reviews}
+                renderItem={renderItem}
                 keyExtractor={item => item.reviewIdx}
                 ListHeaderComponent={
                     <View style={styles.reviewCount}>
-                      <Text style={styles.reviewCountText}>내가 쓴 리뷰 총 {reviewCount}개</Text>
+                        <Text style={styles.reviewCountText}>내가 쓴 리뷰 총 {reviewCount}개</Text>
                     </View>
-                  }
+                }
             />
         </SafeAreaView>
     )
@@ -195,6 +232,8 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 20,
         right: 16,
+    },
+    deleteText: {
         color: Color.lightGray,
         fontSize: 12,
         fontFamily: 'Pretendard-Medium',
