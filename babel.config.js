a60630f22def056cbe19b2a8d178e05e9f8a5e00
vavoya <128780530+vavoya@users.noreply.github.com>
2023-08-14 06:47:44 +0900

    절대 경로 추가

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            pages: './pages',
+            components: './components',
+            assets: './assets',
+            store: './store',
+            utils: './utils'
+          },
+        },
+      ],
+    ],
   };
 };
