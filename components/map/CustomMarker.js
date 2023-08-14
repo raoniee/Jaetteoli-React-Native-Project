4d16e69b5d17d68384e662f0f1c49bceeb76200c
Anwooseong <aws5624@naver.com>
2023-08-15 02:57:34 +0900

    마커 이미지 추가 & 마커 수정

--- components/map/CustomMarker.js
+++ components/map/CustomMarker.js
@@ -3,14 +3,45 @@ import Color from "../../assets/colors/Color";
 import MapMarker from "../../assets/images/MapMarker";
 import { View } from "react-native";
 
-const CustomMarker = ({ title }) => (
-  <Pressable>
-    <View style={styles.container}>
-      <Text style={styles.text}>{title}</Text>
-      <MapMarker />
-    </View>
-  </Pressable>
-);
+const CustomMarker = ({ title, storeCategory }) => {
+  let icon = <Image source={require("../../assets/images/Mart.png")} />;
+  switch (storeCategory) {
+    case 1: //백화점
+      icon = (
+        <Image source={require("../../assets/images/DepartmentStore.png")} />
+      );
+      break;
+    case 2: //편의점
+      icon = (
+        <Image source={require("../../assets/images/ConvenienceStore.png")} />
+      );
+      break;
+    case 3: //디저트
+      icon = <Image source={require("../../assets/images/Dessert.png")} />;
+      break;
+    case 4: //샐러드
+      icon = <Image source={require("../../assets/images/Salad.png")} />;
+      break;
+    case 5: //초밥
+      icon = <Image source={require("../../assets/images/Sushi.png")} />;
+      break;
+    case 6: //카페
+      icon = <Image source={require("../../assets/images/Coffee.png")} />;
+      break;
+    default: //대형마트
+      icon = <Image source={require("../../assets/images/Mart.png")} />;
+      break;
+  }
+  return (
+    <Pressable>
+      <View style={styles.container}>
+        <Text style={styles.text}>{title}</Text>
+        {/* <MapMarker /> */}
+        {icon}
+      </View>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
