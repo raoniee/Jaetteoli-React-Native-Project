c48e07cf6e118a4d9160f0cb241cc534bdff597a
vavoya <128780530+vavoya@users.noreply.github.com>
2023-08-19 02:14:13 +0900

    너비 추가

--- utils/dimensions.js
+++ utils/dimensions.js
@@ -3,5 +3,6 @@ import {Dimensions, Platform} from "react-native";
 
 export const statusBarHeight = Constants.statusBarHeight;
 export const windowHeight = Dimensions.get('window').height
+export const windowWidth = Dimensions.get('window').width
 
-export const totalHeight = Platform.OS === 'ios' ? windowHeight - statusBarHeight : windowHeight;
+export const totalHeight = Platform.OS === 'ios' ? windowHeight - statusBarHeight : windowHeight
