2b12deb2e391920562d8357bc78f44b7be0b7d25
vavoya <128780530+vavoya@users.noreply.github.com>
2023-08-20 09:26:51 +0900

    Text, Image props 수정

--- components/map/CustomMarker.js
+++ components/map/CustomMarker.js
@@ -7,35 +7,34 @@ const CustomMarker = ({ title, storeCategory }) => {
   let icon = <Image source={require("../../assets/images/Mart.png")} />;
   switch (storeCategory) {
     case 1: //백화점
-      icon = (
-        <Image source={require("../../assets/images/DepartmentStore.png")} />
-      );
+      icon = <Image source={require("../../assets/images/DepartmentStore.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
     case 2: //편의점
-      icon = (
-        <Image source={require("../../assets/images/ConvenienceStore.png")} />
-      );
+      icon = <Image source={require("../../assets/images/ConvenienceStore.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
     case 3: //디저트
-      icon = <Image source={require("../../assets/images/Dessert.png")} />;
+      icon = <Image source={require("../../assets/images/Dessert.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
     case 4: //샐러드
-      icon = <Image source={require("../../assets/images/Salad.png")} />;
+      icon = <Image source={require("../../assets/images/Salad.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
     case 5: //초밥
-      icon = <Image source={require("../../assets/images/Sushi.png")} />;
+      icon = <Image source={require("../../assets/images/Sushi.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
     case 6: //카페
-      icon = <Image source={require("../../assets/images/Coffee.png")} />;
+      icon = <Image source={require("../../assets/images/Coffee.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
     default: //대형마트
-      icon = <Image source={require("../../assets/images/Mart.png")} />;
+      icon = <Image source={require("../../assets/images/Mart.png")} style={{width: 37, height: 46}} resizeMode="contain"/>;
       break;
   }
   return (
     <Pressable>
       <View style={styles.container}>
-        <Text style={styles.text}>{title}</Text>
+        <Text style={[styles.text]}>{title}</Text>
+        <Text style={[styles.text, styles.abs, {textShadowOffset: {width: -1, height: -1}}]}>{title}</Text>
+        <Text style={[styles.text, styles.abs, {textShadowOffset: {width: -1, height: 1}}]}>{title}</Text>
+        <Text style={[styles.text, styles.abs, {textShadowOffset: {width: 1, height: -1}}]}>{title}</Text>
         {/* <MapMarker /> */}
         {icon}
       </View>
@@ -53,7 +52,20 @@ const styles = StyleSheet.create({
     fontSize: 14,
     textAlign: "center",
     color: Color.purple,
+    textShadowColor: 'white',
+    textShadowRadius: 1,
+    textShadowOffset: {
+      width: 1,
+      height: 1,
+    },
   },
+  abs: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+  }
 });
 
 export default CustomMarker;
